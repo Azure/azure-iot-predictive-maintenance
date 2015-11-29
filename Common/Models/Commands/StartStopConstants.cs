0f95811417022b456f18f04be1aa1005442dd1fd
clecompt <clecompt@microsoft.com>
2015-11-29 13:53:07 -0800

    Add table storage feedback for simulator state.

---
+++ Common/Models/Commands/StartStopConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Azure.Devices.Applications.PredictiveMaintenance.Common.Models.Commands
+{
+    public class StartStopConstants
+    {
+        public const string STARTING = "Starting";
+        public const string STARTED = "Started";
+        public const string STOPPING = "Stopping";
+        public const string STOPPED = "Stopped";
+    }
+}
---
+++ Common/Models/StateTableEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Devices.Applications.PredictiveMaintenance.Common.Helpers;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Applications.PredictiveMaintenance.Common.Models
+{
+    public class StateTableEntity : TableEntity
+    {
+        public string State { get; set; }
+
+        public static async Task Write(string device, string state, string connectionString, string table)
+        {
+            StateTableEntity entry = new StateTableEntity()
+            {
+                PartitionKey = device,
+                RowKey = "State", // Arbitrary constant; we're only storing one value per device
+                State = state,
+                ETag = "*"
+            };
+
+            // We don't need a data model to represent the result of this operation,
+            // so we use a stub table/model convertor
+            await AzureTableStorageHelper.DoTableInsertOrReplaceAsync<object, StateTableEntity>(entry, (StateTableEntity e) => { return null; }, connectionString, table);
+        }
+    }
+}
