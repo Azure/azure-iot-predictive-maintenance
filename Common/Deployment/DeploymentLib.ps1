26e593384fadd49be69e05188a266ae82ecebd07
Douglas Richardson <dricha@microsoft.com>
2016-02-10 10:12:58 -0800

    Added 3 more locations, formalized ML region fallback, added seperate version file, add numbered option selection

--- Common/Deployment/DeploymentLib.ps1
+++ Common/Deployment/DeploymentLib.ps1
@@ -130,17 +130,39 @@ function SendRequest()
 
 function GetSuiteLocation()
 {
-    $command = "Read-Host 'Enter Region to deploy resources (eg. East US)'"
-    Write-Host
-    Write-Host "Available Locations:";
+    Write-Host "Available locations:";
+    $regions = @();
+    $index = 1
     foreach ($loc in $locations)
     {
-        Write-Host $loc
+        $region = New-Object System.Object
+        $region | Add-Member -MemberType NoteProperty -Name "Option" -Value $index
+        $region | Add-Member -MemberType NoteProperty -Name "Region" -Value $loc
+        $regions += $region
+        $index += 1
     }
-    $region = Invoke-Expression $command
-    while (!(ValidateLocation $region))
+    
+    Write-Host ($regions | Out-String)
+    
+    $region = "notset"
+    while ($region -eq "notset" -or !(ValidateLocation $region))
     {
-        $region = Invoke-Expression $command
+        try 
+        {
+            [int]$selectedIndex = Read-Host 'Select an option from the above list'
+        }
+        catch 
+        {
+            Write-Host "Must be a number"
+            continue
+        }
+        
+        if ($selectedIndex -lt 1 -or $selectedIndex -ge $index)
+        {
+            continue
+        }
+        
+        $region = $locations[$selectedIndex - 1]
     }
     return $region
 }
@@ -148,6 +170,7 @@ function GetSuiteLocation()
 function ValidateLocation()
 {
     param ([Parameter(Mandatory=$true)][string]$location)
+        
     foreach ($loc in $global:locations)
     {
         if ($loc.Replace(' ', '').ToLowerInvariant() -eq $location.Replace(' ', '').ToLowerInvariant())
@@ -421,7 +444,7 @@ function UploadFile()
         $sasToken = $blob.GetSharedAccessSignature($sasPolicy)
     }
     return $blob.Uri.ToString() + $sasToken
-}
+    }
 
 function CreateMLWorkSpace()
 {
@@ -432,12 +455,7 @@ function CreateMLWorkSpace()
         [Parameter(Mandatory=$true, Position=1)]
         [string]$resourceGroup
     )
-    $mlLocation = "South Central US"
-    switch($global:AllocationRegion)
-    {
-        "North Europe"{$mlLocation = "West Europe"}
-        "East Asia"{$mlLocation = "Southeast Asia"}
-}
+    $mlLocation = $global:mlRegionFallback.Get_Item($global:AllocationRegion)
     $subscription = Get-AzureSubscription -SubscriptionId $global:SubscriptionId
     $storageAccount = GetAzureStorageAccount $("ml" + $name) $resourceGroupName $mlLocation
     $storageAccountKey = (Get-AzureRmStorageAccountKey -StorageAccountName $storageAccount.StorageAccountName -ResourceGroupName $resourceGroupName).Key1
@@ -875,8 +893,9 @@ function GetAADTenant()
     else
     {
         # List Active directories associated with account
-        Write-Host "Available Active directories:"
+        Write-Host "Available Active Directories:"
         $directories = @()
+        $index = 1
         foreach ($tenantObj in $tenants)
         {
             $tenant = $tenantObj.TenantId
@@ -887,19 +906,28 @@ function GetAADTenant()
             if ($result -ne $null)
             {
                 $directory = New-Object System.Object
+                $directory | Add-Member -MemberType NoteProperty -Name "Option" -Value $index
                 $directory | Add-Member -MemberType NoteProperty -Name "Directory Name" -Value ($result.userPrincipalName.Split('@')[1])
                 $directory | Add-Member -MemberType NoteProperty -Name "Tenant Id" -Value $tenant
                 $directories += $directory
+                $index += 1
             }
         }
 
-        # Can't determine AADTenant, so prompt
-        [string]$tenantId = "notset"
+        [int]$selectedIndex = -1
         write-host ($directories | Out-String)
-        while (!(($tenants | ?{$_.TenantId -eq $tenantId}) -ne $null))
+        while ($selectedIndex -lt 1 -or $selectedIndex -ge $index)
         {
-            [string]$tenantId = Read-Host "Please select a valid TenantId from list"
+            try
+            {
+                [int]$selectedIndex = Read-Host "Select an option from the above list"
+            }
+            catch
+            {
+                Write-Host "Must be a number"
+            }
         }
+        $tenantId = $tenants[$selectedIndex - 1].TenantId
     }
     
     # Configure Application
@@ -1004,26 +1032,44 @@ function InitializeEnvironment()
         $global:envSettingsXml = [xml](cat $global:environmentSettingsFile)
     }
 
-    if (!(Test-Path variable:SubscriptionId))
+    if ([string]::IsNullOrEmpty($global:SubscriptionId))
     {
         $accounts = Get-AzureRmSubscription
         $global:SubscriptionId = GetEnvSetting "SubscriptionId"
+        
         if ([string]::IsNullOrEmpty($global:SubscriptionId))
         {
-            $global:SubscriptionId = "z"
-        }
-        while (!$accounts.SubscriptionId.Contains($global:SubscriptionId))
-        {
             Write-Host "Available subscriptions:"
-            $accounts |ft SubscriptionName, SubscriptionId -au
-            $global:SubscriptionId = Read-Host "Please select a valid SubscriptionId from list"
+                $global:index = 0
+                $selectedIndex = -1
+                $accounts | Format-Table -Property @{name="Option";expression={$global:index;$global:index+=1}},SubscriptionName, SubscriptionId -au
+            
+            while (!$accounts.SubscriptionId.Contains($global:SubscriptionId))
+            {
+                try
+                {
+                    [int]$selectedIndex = Read-Host "Select an option from the above list"
+                }
+                catch
+                {
+                    Write-Host "Must be a number"
+                    continue
+                }
+                
+                if ($selectedIndex -lt 1 -or $selectedIndex -gt $accounts.length)
+                {
+                    continue
+                }
+                
+                $global:SubscriptionId = $accounts[$selectedIndex - 1].SubscriptionId
+            }
+            UpdateEnvSetting "SubscriptionId" $global:SubscriptionId
         }
-        UpdateEnvSetting "SubscriptionId" $global:SubscriptionId
     }
     Select-AzureSubscription -SubscriptionId $global:SubscriptionId
     Select-AzureRmSubscription -SubscriptionId $global:SubscriptionId
 
-    if (!(Test-Path variable:AllocationRegion))
+    if ([string]::IsNullOrEmpty($global:AllocationRegion))
     {
         $global:AllocationRegion = GetOrSetEnvSetting "AllocationRegion" "GetSuiteLocation"
     }
@@ -1074,12 +1120,23 @@ $global:timeStampFormat = "o"
 $global:resourceNotFound = "ResourceNotFound"
 $global:serviceNameToken = "ServiceName"
 $global:azurePath = Split-Path $MyInvocation.MyCommand.Path
-$global:version = "v1.1.0"
+$global:version = Get-Content ("{0}\..\..\VERSION.txt" -f $global:azurePath)
 $global:azureVersion = "1.0.3"
 $global:aadLoginUrl = "https://login.windows.net/"
 $global:azureUrl = "https://management.core.windows.net/"
 $global:studioApiUrl = "https://studioapi.azureml.net/"
-$global:locations = @("East US", "North Europe", "East Asia")
+$global:locations = @("East US", "North Europe", "East Asia", "West US", "West Europe", "South East Asia")
+
+# Machine Learning is only available in a subset of regions, so we need to do fallback to the correct region
+# See CreateMLWorkSpace for usage
+$global:mlRegionFallback = @{
+    "East US"           = "South Central US";
+    "West US"           = "South Central US";
+    "North Europe"      = "West Europe";
+    "West Europe"       = "West Europe";
+    "East Asia"         = "South East Asia";
+    "South East Asia"   = "South East Asia";
+}
 
 # Check version
 $module = Get-Module -ListAvailable | Where-Object{ $_.Name -eq 'Azure' }
--- build.ps1
+++ build.ps1
@@ -30,11 +30,11 @@ function Build() {
 
 # Package - see :Package in build.cmd
 function Package() {
-    &$msbuild ("Web\Web.csproj", "/v:m", "/t:Package")
+    &$msbuild ("Web\Web.csproj", "/v:m", "/p:Configuration=$configuration", "/t:Package")
     If (-Not($?)) {
         throw "Packaging Web\Web.csproj failed"
     }
-    &$msbuild ("WebJobHost\WebJobHost.csproj", "/v:m", "/t:Package")
+    &$msbuild ("WebJobHost\WebJobHost.csproj", "/v:m", "/p:Configuration=$configuration", "/t:Package")
     If (-Not($?)) {
         throw "Packaging WebJobHost\WebJobHost.csproj failed"
     }
