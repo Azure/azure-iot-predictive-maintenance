45573dcd7c8e1774276515635bd57cacfa6cab2a
alzaslon <alzaslon@microsodt.com>
2016-04-20 15:23:09 -0700

    Added localization.

---
+++ Web/ViewModels/LanguageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Azure.Devices.Applications.PredictiveMaintenance.Web.ViewModels
+{
+    public sealed class LanguageModel
+    {
+        public string Name { get; set; }
+
+        public string CultureName { get; set; }
+    }
+}
