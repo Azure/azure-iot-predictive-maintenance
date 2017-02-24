cf63e529bd5af9d7cdf3fb8f07eea8836a4b4959
Hui Jiang <hujiang@microsoft.com>
2017-02-24 17:07:55 +0800

    Revert the output

--- Common/Deployment/Rdeploy.ps1
+++ Common/Deployment/Rdeploy.ps1
@@ -14,8 +14,7 @@ Expand-Archive -Path $zipfile -Force
 cd $folderName
 $args = ("ConfigMRS.dll", "-setpassword", $password, "-deployrpath", "`"$deployrPath`"")
 
-#Start-Process -FilePath "dotnet" -ArgumentList $args -WindowStyle Hidden
-Start-Process -FilePath "dotnet" -ArgumentList $args -WindowStyle Hidden -RedirectStandardOutput "out.log"  
+Start-Process -FilePath "dotnet" -ArgumentList $args -WindowStyle Hidden
 cd ..
 
 exit 0
